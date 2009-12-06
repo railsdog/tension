120c286eb5749b741b666234541dd3cf2295b4bd
Brian Quinn <briandquinn@gmail.com>
2009-12-06 21:39:17 +0000

    Added deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+set :application, "spreedirectory"
+
+set :scm, :git
+set :repository,  "git://github.com/railsdog/tension.git"
+set :branch, "master"
+
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+set :git_shallow_clone, 1
+set :keep_releases, 5
+
+role :web, "217.115.112.221"                   # Your HTTP server, Apache/etc
+role :app, "217.115.112.221"                   # This may be the same as your `Web` server
+role :db,  "217.115.112.221", :primary => true # This is where Rails migrations will run
+
+set :use_sudo, false
+set :user, "railsdog"
+
+ssh_options[:forward_agent] = true
+
+set :deploy_to, "/data/apps/#{application}"
+
+namespace :deploy do
+  task :start do; end
+  task :stop do; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
